2ddc8be7e18978d1b1936190f661d4680b997b4c
Hamidou NDIAYE <hamidoundiaye9853@gmail.com>
2026-09-06 00:42:42 +0200

    [INIT] Initialisation du projet Spring Boot Thymeleaf

---
+++ src/test/java/com/rplace/RPlaceClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RPlaceClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
