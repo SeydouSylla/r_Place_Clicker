7f3d0fe9b28310ea378144279a9920234bcf0a7e
Seydou Sylla <seydousylla180@gmail.com>
2026-09-06 16:14:56 +0200

    feat: Ajout enumeration CategorieBonus

---
+++ src/main/java/com/rplace/enumeration/CategorieBonus.java
@@ -0,0 +1,11 @@
+package com.rplace.enumeration;
+
+/**
+ * Categories possibles pour un bonus Cookie Clicker.
+ * GENERATEUR : produit des credits automatiquement (curseur, grand-mere...)
+ * CAPACITE   : debloque une fonctionnalite (couleurs, pinceau large)
+ */
+public enum CategorieBonus {
+    GENERATEUR,
+    CAPACITE
+}
